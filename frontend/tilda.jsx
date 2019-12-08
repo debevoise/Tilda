d3ccb06cc54b11f77bb95573bcf092e6db2fab57
Simon_DeBevoise <simondebevoise@gmail.com>
2019-12-07 21:32:51 -0500

    added reset.css and basic styling

--- frontend/tilda.jsx
+++ frontend/tilda.jsx
@@ -1,3 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+document.addEventListener('DOMContentLoaded', () => {
+    const root = document.getElementById('root');
+
+    let dummyHTML = <h3>Tilda React in Place</h3>;
+
+    ReactDOM.render(dummyHTML, root);
+});
